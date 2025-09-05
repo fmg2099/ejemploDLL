8055a828f1c8c4f682d1c8e3270bccb6d7596dcc
Lug00 <luis.lugo@saei.mx>
2025-09-05 14:30:49 -0600

    funciones de messagebox

--- EjemploDLL/MyLibrary.cpp
+++ EjemploDLL/MyLibrary.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "MyLibrary.h"
+#include "curl/curl.h"
 
 MYLIBRARY_API void HelloWorld()
 {
@@ -14,4 +15,59 @@ MYLIBRARY_API int MyMessageBoxError(const wchar_t* message, const wchar_t * boxT
 MYLIBRARY_API int MyMessageBoxWarning(const wchar_t* message, const wchar_t * boxTitle)
 {
 	return MessageBoxW(NULL, message, boxTitle, MB_ICONEXCLAMATION);
+}
+
+MYLIBRARY_API int HTTPGet(const char* url, WriteCallback callback)
+{
+	//inicializa curl
+	CURL* curl;
+	CURLcode res; 
+	
+	curl = curl_easy_init();
+
+	if (curl)
+	{
+		std::string response;
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunction);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, callback);
+
+		res = curl_easy_perform(curl);
+		if (res != CURLE_OK)
+		{
+			std::cerr << "Error al hacer la peticion: " << curl_easy_strerror(res) << std::endl;
+			return -1;
+		}
+		else
+		{
+			////llama a la funcion callback del usuario
+			//if (callback)
+			//{
+			//	callback(response.c_str());
+			//}
+			std::cout << "peticion completada" << std::endl;
+		}
+		curl_easy_cleanup(curl);
+
+	}
+}
+
+//definicion de la funcion callback para curl
+size_t writefunction(void* ptr, size_t size, size_t nmemb, void* userdata)
+{
+	//total de bytes que se descargaron
+	size_t totalSize = size * nmemb;
+
+	////llama a la funcion callback del usuario
+	WriteCallback userCallback = reinterpret_cast<WriteCallback>(userdata);
+	if (userCallback)
+	{
+		userCallback(  "hola desde la DLL :D");
+	}
+	else
+	{
+		std::cout << "no se pudo hacer el cast" << std::endl;
+	}
+	std::cout << "recibidos " << totalSize << " bytes" << std::endl;
+	return totalSize;
 }
--- EjemploDLL/MyLibrary.h
+++ EjemploDLL/MyLibrary.h
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <iostream>
 
+
 //indica al compilador que la funcion se exporta en la DLL
 #ifdef MYLIBRARY_EXPORTS
 #define MYLIBRARY_API __declspec(dllexport)
@@ -12,11 +13,20 @@
 #define MYLIBRARY_API __declspec(dllimport)
 #endif
 
+//definicion de la funcion callback para HTTPGet
+typedef size_t(*WriteCallback) (const char* message);
+
+//prototipo de la funcion callback para curl 
+size_t writefunction(void* ptr, size_t size, size_t nmemb, void* userdata);
+
 extern "C"
 {
 	MYLIBRARY_API void HelloWorld();
 	//Mostrar caja de mensaje de la aPI de windows
 	MYLIBRARY_API int MyMessageBoxError(const wchar_t* message, const wchar_t* boxTitle);
 	MYLIBRARY_API int MyMessageBoxWarning(const wchar_t* message, const wchar_t* boxTitle);
+
+	//get requires a fucntion callback
+	MYLIBRARY_API int HTTPGet(const char* url, WriteCallback callback);
 
 }
